209f9bb066de43499c8fb16244e86ab2d49b0dc3
sebas13110 <57629919+sebas13110@users.noreply.github.com>
2021-06-07 16:22:14 -0500

    Comentario en la linea 234
    ola

--- src/hotel/Login.java
+++ src/hotel/Login.java
@@ -231,4 +231,5 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JTextField User;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
+    //holaamiwos
 }
