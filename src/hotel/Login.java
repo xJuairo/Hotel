18a09f2b4ae0be5f33cfe72faa2ea32b744f95d5
SeergioSolis <83850547+SeergioSolis@users.noreply.github.com>
2021-06-11 17:55:58 -0500

    REVISION Y AGREGADO DE PRESENTACION DE LEMA Y SLOGAN

--- src/hotel/Login.java
+++ src/hotel/Login.java
@@ -23,6 +23,7 @@ import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
+
 /**
  *
  * @author xjlop
@@ -34,7 +35,7 @@ public class Login extends javax.swing.JFrame {
      */
     Conexion cc = new Conexion();
     Connection cn = cc.conexion();
-    public Login() {
+    public Login()  {
         initComponents();
         this.dispose();
         this.setUndecorated(true);
@@ -200,30 +201,7 @@ public class Login extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
+       java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Login().setVisible(true);
             }
@@ -234,6 +212,10 @@ public class Login extends javax.swing.JFrame {
         m.update(s.getBytes(),0,s.length());     
         return new BigInteger(1,m.digest()).toString(16); 
    }
+   
+   
+
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Ingresar;
     private javax.swing.JPasswordField Password;
